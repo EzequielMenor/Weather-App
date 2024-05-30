e7493b13b952f1accb977603aacd3c0e83b0a625
Ezequiel Menor <ezequielmenor@gmail.com>
2024-05-30 09:12:00 +0200

    navbar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,113 @@
 /** @format  */
+'use client'
 
-import Navbar from './components/Navbar'
+import { useQuery } from 'react-query'
+import Navbar from '../components/Navbar'
+import axios from 'axios'
+import { parseISO, format } from 'date-fns'
+
+//  https://api.openweathermap.org/data/2.5/forecast?q=${place}&appid=${process.env.NEXT_PUBLIC_WEATHER_KEY}&cnt=56
+
+interface WeatherDetail {
+  dt: number
+  main: {
+    temp: number
+    feels_like: number
+    temp_min: number
+    temp_max: number
+    pressure: number
+    sea_level: number
+    grnd_level: number
+    humidity: number
+    temp_kf: number
+  }
+  weather: {
+    id: number
+    main: string
+    description: string
+    icon: string
+  }[]
+  clouds: {
+    all: number
+  }
+  wind: {
+    speed: number
+    deg: number
+    gust: number
+  }
+  visibility: number
+  pop: number
+  sys: {
+    pod: string
+  }
+  dt_txt: string
+}
+
+interface WeatherData {
+  cod: string
+  message: number
+  cnt: number
+  list: WeatherDetail[]
+  city: {
+    id: number
+    name: string
+    coord: {
+      lat: number
+      lon: number
+    }
+    country: string
+    population: number
+    timezone: number
+    sunrise: number
+    sunset: number
+  }
+}
 
 export default function Home() {
+  const { isLoading, error, data } = useQuery<WeatherData>(
+    'repoData',
+    async () => {
+      const { data } = await axios.get(
+        `https://api.openweathermap.org/data/2.5/forecast?q=pune&appid=${process.env.NEXT_PUBLIC_WEATHER_KEY}&cnt=56`
+      )
+      return data
+    }
+    // fetch(
+    //   'https://api.openweathermap.org/data/2.5/forecast?q=pune&appid=cb92244fb79ae05fdaa562549435d6bc&cnt=56'
+    // ).then((res) => res.json())
+  )
+
+  const firstData = data?.list[0]
+
+  console.log('data', data?.city.name)
+
+  if (isLoading)
+    return (
+      <div className="flex items-center min-h-screen justify-center">
+        <p className="animate-bounce">Loading...</p>
+      </div>
+    )
+
   return (
     <div className="flex flex-col gap-4 bg-gray-100 min-h-screen">
       <Navbar />
+      <main className="px-3 max-w-7xl mx-auto flex flex-col gap-9 w-full pb-10 pt-4">
+        {/* today data */}
+        <section>
+          <div>
+            <div className="flex gap-1 text-2xl items-end">
+              <p>{format(parseISO(firstData?.dt_txt ?? ''), 'EEEE')}</p>
+              <p className="text-lg">
+                ({format(parseISO(firstData?.dt_txt ?? ''), 'dd.MM.yy')})
+              </p>
+            </div>
+            <div></div>
+          </div>
+        </section>
+
+        {/* 7 day forcast */}
+        <section></section>
+      </main>
     </div>
   )
 }
---
+++ src/components/container.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+type Props = {}
+
+export default function container({}: Props) {
+  return (
+    <div className="w-full bg-white border rounded-xl flex py-4 shadow-sm">Container</div>
+  )
+}
